431fb832b33b8aa73fe0640f1068025d2f944241
Roel van Hintum <roel@born05.com>
2019-10-08 09:43:01 +0200

    Update SentryService.php

--- src/services/SentryService.php
+++ src/services/SentryService.php
@@ -14,44 +14,25 @@ use yii\base\Exception;
 
 class SentryService extends Component
 {
-    // Public Properties
-    // =========================================================================
-
-    public $app;
-
-    public $info;
-
-    public $plugin;
-
-    public $settings;
-
-    // Public Methods
-    // =========================================================================
-
-    public function init()
-    {
-        parent::init();
-
-        $this->app = Craft::$app;
-        $this->info = $this->app->getInfo();
-        $this->plugin = SentryPlugin::$plugin;
-        $this->settings = $this->plugin->getSettings();
-    }
-
     public function handleException($exception)
     {
-        if (!$this->settings->enabled) {
-            Craft::info('Exception not reported to Sentry as the plugin is disabled.', $this->plugin->handle);
+        $app = Craft::$app;
+        $info = $app->getInfo();
+        $plugin = SentryPlugin::$plugin;
+        $settings = $plugin->getSettings();
+
+        if (!$settings->enabled) {
+            Craft::info('Exception not reported to Sentry as the plugin is disabled.', $plugin->handle);
             return;
         }
 
-        if (!$this->settings->clientDsn) {
-            Craft::error('Failed to report exception due to missing client key (DSN)', $this->plugin->handle);
+        if (!$settings->clientDsn) {
+            Craft::error('Failed to report exception due to missing client key (DSN)', $plugin->handle);
             return;
         }
 
-        $dsn = $this->settings->clientDsn ? Craft::parseEnv($this->settings->clientDsn) : null;
-        $environment = $this->settings->environment ? Craft::parseEnv($this->settings->environment) : null;
+        $dsn = $settings->clientDsn ? Craft::parseEnv($settings->clientDsn) : null;
+        $environment = $settings->environment ? Craft::parseEnv($settings->environment) : null;
 
         // If this is a Twig Runtime exception, use the previous one instead
         if ($exception instanceof \Twig_Error_Runtime && ($previousException = $exception->getPrevious()) !== null) {
@@ -59,10 +40,10 @@ class SentryService extends Component
         }
 
         $statusCode = $exception->statusCode ?? null;
-        $excludedStatusCodes = array_map('trim', explode(',', $this->settings->excludedCodes));
+        $excludedStatusCodes = array_map('trim', explode(',', $settings->excludedCodes));
 
         if (in_array($statusCode, $excludedStatusCodes)) {
-            Craft::info('Exception status code excluded from being reported to Sentry.', $this->plugin->handle);
+            Craft::info('Exception status code excluded from being reported to Sentry.', $plugin->handle);
             return;
         }
 
@@ -73,8 +54,8 @@ class SentryService extends Component
 
         $user = Craft::$app->getUser()->getIdentity();
 
-        Sentry\configureScope(function (Scope $scope) {
-            if (isset($user)) {
+        Sentry\configureScope(function (Scope $scope) use ($app, $info, $plugin, $settings, $user, $statusCode) {
+            if ($user) {
                 $scope->setUser([
                     'ID'       => $user->id,
                     'Username' => $user->username,
@@ -84,11 +65,11 @@ class SentryService extends Component
             }
 
             $scope->setExtra('App Type', 'Craft CMS');
-            $scope->setExtra('App Name', $this->info->name);
-            $scope->setExtra('App Edition (licensed)', $this->app->getLicensedEditionName());
-            $scope->setExtra('App Edition (running)', $this->app->getEditionName());
-            $scope->setExtra('App Version', $this->info->version);
-            $scope->setExtra('App Version (schema)', $this->info->schemaVersion);
+            $scope->setExtra('App Name', $info->name);
+            $scope->setExtra('App Edition (licensed)', $app->getLicensedEditionName());
+            $scope->setExtra('App Edition (running)', $app->getEditionName());
+            $scope->setExtra('App Version', $info->version);
+            $scope->setExtra('App Version (schema)', $info->schemaVersion);
             $scope->setExtra('PHP Version', phpversion());
             $scope->setExtra('Status Code', $statusCode);
         });
