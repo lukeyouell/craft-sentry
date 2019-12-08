ab6fb227300953064a0143ff1c2ac232e535f0d6
lukeyouell <lukeyouell@imac.lan>
2019-12-08 13:23:24 +0000

    Changes to utilise Sentry 2.2

--- src/services/SentryService.php
+++ src/services/SentryService.php
@@ -7,9 +7,6 @@ use craft\base\Component;
 
 use lukeyouell\sentry\Sentry;
 
-use Raven_Client;
-use Raven_ErrorHandler;
-
 use yii\base\Exception;
 
 class SentryService extends Component
@@ -28,76 +25,78 @@ class SentryService extends Component
     // Public Methods
     // =========================================================================
 
-    public function init()
-    {
-        parent::init();
-
-        $this->app = Craft::$app;
-        $this->info = $this->app->getInfo();
-        $this->plugin = Sentry::$plugin;
-        $this->settings = $this->plugin->getSettings();
-    }
-
     public function handleException($exception)
     {
-        if (!$this->settings->enabled) {
-            Craft::info('Exception not reported to Sentry as the plugin is disabled.', $this->plugin->handle);
-            return;
-        }
-
-        if (!$this->settings->clientDsn) {
-            Craft::error('Failed to report exception due to missing client key (DSN)', $this->plugin->handle);
-            return;
-        }
-
-        $dsn = $this->settings->clientDsn ? Craft::parseEnv($this->settings->clientDsn) : null;
-        $environment = $this->settings->environment ? Craft::parseEnv($this->settings->environment) : null;
-
         // If this is a Twig Runtime exception, use the previous one instead
         if ($exception instanceof \Twig_Error_Runtime && ($previousException = $exception->getPrevious()) !== null) {
             $exception = $previousException;
         }
 
-        $statusCode = $exception->statusCode ?? null;
-        $excludedStatusCodes = array_map('trim', explode(',', $this->settings->excludedCodes));
+        if ($this->canReport($exception->statusCode))
+        {
+            $dsn = Sentry::$plugin->getSettings()->getClientDsn();
+            $environment = Sentry::$plugin->getSettings()->getEnvironment();
 
-        if (in_array($statusCode, $excludedStatusCodes)) {
-            Craft::info('Exception status code excluded from being reported to Sentry.', $this->plugin->handle);
-            return;
-        }
-
-        $client = new Raven_Client($dsn);
-        $handler = new Raven_ErrorHandler($client);
-        $handler->registerExceptionHandler();
-        $handler->registerErrorHandler();
-        $handler->registerShutdownFunction();
-
-        $user = Craft::$app->getUser()->getIdentity();
-
-        if ($user) {
-            $client->user_context([
-                'ID'       => $user->id,
-                'Username' => $user->username,
-                'Email'    => $user->email,
-                'Admin'    => $user->admin ? 'Yes' : 'No',
+            \Sentry\init([
+                'dsn' => $dsn,
+                'environment' => $environment,
             ]);
+
+            \Sentry\configureScope(function (\Sentry\State\Scope $scope) use ($exception): void {
+                $user = Craft::$app->getUser()->getIdentity();
+
+                if ($user)
+                {
+                    $scope->setUser([
+                        'id'       => $user->id,
+                        'username' => $user->username,
+                        'admin'    => $user->admin ? 'Yes' : 'No',
+                    ]);
+                }
+
+                $scope->setTag('app', 'Craft CMS');
+                $scope->setTag('status_code', $exception->statusCode);
+
+                $scope->setExtra('Craft Name', Craft::$app->getInfo()->name);
+                $scope->setExtra('Craft Edition (licensed)', Craft::$app->getLicensedEditionName());
+                $scope->setExtra('Craft Edition (running)', Craft::$app->getEditionName());
+                $scope->setExtra('Craft Version', Craft::$app->getInfo()->version);
+                $scope->setExtra('Craft Schema Version', Craft::$app->getInfo()->schemaVersion);
+            });
+
+            \Sentry\captureException($exception);
+        }
+    }
+    
+    private function canReport($statusCode = null, $dsn = null)
+    {
+        $canReport = true;
+        $message = null;
+        $excludedStatusCodes = Sentry::$plugin->getSettings()->getExcludedStatusCodes();
+
+        if (!Sentry::$plugin->getSettings()->enabled) {        
+            $canReport = false;
+            $message = 'Exception not reported to Sentry as the plugin is disabled.';
         }
 
-        if ($environment) {
-            $client->setEnvironment($environment);
+        if (!Sentry::$plugin->getSettings()->getClientDsn()) {
+            $canReport = false;
+            $message = 'Failed to report exception due to missing client key (DSN)';
         }
 
-        $client->captureException($exception, [
-            'extra' => [
-                'App Type'               => 'Craft CMS',
-                'App Name'               => $this->info->name,
-                'App Edition (licensed)' => $this->app->getLicensedEditionName(),
-                'App Edition (running)'  => $this->app->getEditionName(),
-                'App Version'            => $this->info->version,
-                'App Version (schema)'   => $this->info->schemaVersion,
-                'PHP Version'            => phpversion(),
-                'Status Code'            => $statusCode,
-            ],
-        ]);
+        if ($statusCode && $excludedStatusCodes)
+        {
+            if (in_array($statusCode, $excludedStatusCodes)) {
+                $canReport = false;
+                $message = 'Exception status code excluded from being reported to Sentry.';
+            }
+        }
+
+        if (!$canReport)
+        {
+            Craft::error($message, Sentry::$plugin->handle);
+        }
+
+        return $canReport;
     }
 }
